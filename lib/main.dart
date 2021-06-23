eb5ee919a5f17f678e54e932562abc8b95b6e9ca
TheUltimateOptimist <digitalConfidence@web.de>
2021-06-23 18:57:52 +0200

    updated name

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Conways Spiel des Lebens',
+      title: "Conway's Spiel des Lebens",
       home: MyHomePage(),
     );
   }
